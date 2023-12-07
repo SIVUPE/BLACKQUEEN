c8c8f9131f329a47f013ef58891978a5cda07cfa
SIVUPE <sivuyisiwepeter79@gmail.com>
2023-12-07 17:37:02 +0200

    checkout

--- script/products.js
+++ script/products.js
@@ -10,7 +10,7 @@ let products = JSON.parse(localStorage.getItem('products')) ?
                 "image" :"https://i.postimg.cc/26gcFT8F/Sunny-Isle-Jamaican-Black-Castor-Oil-SHEA-COCONUT-Intensive-Repair-Masque-16oz-rev-23524-removebg-p.png"
             },
             {
-                "id": 2,
+                "id": 2, 
                 "name": "Edge Gel",
                 "amount": 250,
                 "image" :"https://i.postimg.cc/wvMcrNbf/Sunny-Isle-Jamaican-Black-Castor-Oil-Edge-Gel-3-5oz-52359-removebg-preview.png"
@@ -49,7 +49,7 @@ function displayProduct(prododo=null) {
               <div class="card-body">
                 <h5 class="card-title">${product.name}</h5>
                 <p class="card-text">Amount: R${product.amount}</p>
-                <button class="btn" onclick= Add To Cart(${JSON.stringify(product)})>Add To Cart</button>
+                <button class="btn" onclick='addToCart(${JSON.stringify(product)})'>Add To Cart</button>
               </div>
             </div> 
             `;
@@ -108,11 +108,11 @@ search.addEventListener('keyup',() => {
 //ADD TO CART
 
 let Cart = []
-function AddtoCart(item){
-    console.log();
-    if(Cart)
-    Cart.push(item)
-localStorage.setItem('Cart',JSON.stringify(cart))
+function addToCart(item){
+    if(item){
+        Cart.push(item)
+        localStorage.setItem('Cart',JSON.stringify(Cart))
+    }
 }
 
 
